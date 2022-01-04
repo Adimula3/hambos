7bd1d28e643d9e9ef0cfbd10b42dabd3d1a153f6
Joseph Lagbalu <58997596+Jos33y@users.noreply.github.com>
2022-01-04 14:41:29 -0500

    updated indexjs again
    added another update by removing router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    do BrowserRoutercument.getElementById("root")
+        <App />,
+    document.getElementById("root")
 );
